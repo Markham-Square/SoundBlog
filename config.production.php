0a6beefec1187fb9649e59d79300baf3656878f5
Matt Basile <matthewbasile2@gmail.com>
2020-12-08 22:01:13 -0500

    removing index from file name

--- config.production.php
+++ config.production.php
@@ -14,10 +14,10 @@ return [
         'posts' => [
             'author' => 'Dre', // Default author, if not provided in a post
             'sort' => '-date',
-            'path' => '/blog/{filename}/index.html',
+            'path' => '/blog/{filename}',
         ],
         'categories' => [
-            'path' => '/blog/categories/{filename}/index.html',
+            'path' => '/blog/categories/{filename}',
             'posts' => function ($page, $allPosts) {
                 return $allPosts->filter(function ($post) use ($page) {
                     return $post->categories ? in_array($page->getFilename(), $post->categories, true) : false;
