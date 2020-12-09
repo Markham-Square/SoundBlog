9ed0f7d133cc9af5ed74562149912ddc24e30e97
Matt Basile <matthewbasile2@gmail.com>
2020-12-08 21:39:21 -0500

    updating config

--- config.production.php
+++ config.production.php
@@ -6,4 +6,57 @@ return [
     'siteName' => 'SoundBlog',
     'siteDescription' => 'The blog of Soundlog, the app where top publishers unlock their catalog insights.',
     'siteAuthor' => 'SoundLog - Matt Basile',
+
+
+        // collections
+        'collections' => [
+            'posts' => [
+                'author' => 'Dre', // Default author, if not provided in a post
+                'sort' => '-date',
+                'path' => 'blog/{filename}/index.html',
+            ],
+            'categories' => [
+                'path' => '/blog/categories/{filename}/index.html',
+                'posts' => function ($page, $allPosts) {
+                    return $allPosts->filter(function ($post) use ($page) {
+                        return $post->categories ? in_array($page->getFilename(), $post->categories, true) : false;
+                    });
+                },
+            ],
+        ],
+    
+        // helpers
+        'getDate' => function ($page) {
+            return Datetime::createFromFormat('U', $page->date);
+        },
+        'getExcerpt' => function ($page, $length = 255) {
+            if ($page->excerpt) {
+                return $page->excerpt;
+            }
+    
+            $content = preg_split('/<!-- more -->/m', $page->getContent(), 2);
+            $cleaned = trim(
+                strip_tags(
+                    preg_replace(['/<pre>[\w\W]*?<\/pre>/', '/<h\d>[\w\W]*?<\/h\d>/'], '', $content[0]),
+                    '<code>'
+                )
+            );
+    
+            if (count($content) > 1) {
+                return $cleaned;
+            }
+    
+            $truncated = substr($cleaned, 0, $length);
+    
+            if (substr_count($truncated, '<code>') > substr_count($truncated, '</code>')) {
+                $truncated .= '</code>';
+            }
+    
+            return strlen($cleaned) > $length
+                ? preg_replace('/\s+?(\S+)?$/', '', $truncated) . '...'
+                : $cleaned;
+        },
+        'isActive' => function ($page, $path) {
+            return Str::endsWith(trimPath($page->getPath()), trimPath($path));
+        },
 ];
