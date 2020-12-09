26ee1cd53750b60eeb7c042823d50e93103a0868
Matt Basile <matthewbasile2@gmail.com>
2020-12-08 21:34:22 -0500

    updating 404

--- config.production.php
+++ config.production.php
@@ -4,8 +4,6 @@ return [
     'baseUrl' => 'https://soundblog.netlify.app/',
     'production' => true,
     'siteName' => 'SoundBlog',
-    'siteDescription' => 'The blog of Soundlog, the app where top publishers unlock
-    their catalog insights.',
+    'siteDescription' => 'The blog of Soundlog, the app where top publishers unlock their catalog insights.',
     'siteAuthor' => 'SoundLog - Matt Basile',
-    
 ];
